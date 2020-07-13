a36c905bde113e71258fa098eded828ee3905ca8
zscholts <zachscholts@quickenloans.com>
2020-07-13 16:21:20 -0400

    General Code Cleanup / converted (y/n) check to method
    Also added ability to get alternate information for selected student

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace StudentInformation
             const int arrayLength = 11;
             string[] studentNames = {"Charlie", "Frank", "Mac", "Dennis", "Artemis", "Cricket", "Liam", "Ryan", "Waitress", "Dee", "Bill"};
             string[] studentFood = {"cheese", "rum ham", "beer", "pizza", "jagerbombs", "lemons", "milk", "milk", "coffee", "trash cake", "drugs" };
-            string[] studentOldTitle = {"janitor", "duper", "muscle", "psychopath", "", "priest", "kidnapper", "Liam's shadow", "barista", "bird", "football player" };
+            string[] studentOldTitle = {"janitor", "duper", "muscle", "psychopath", "drunk", "priest", "kidnapper", "Liam's shadow", "barista", "bird", "football player" };
 
             bool continueFlag = true;
             Console.WriteLine("Welcome to our Dev.Build class!\n");
@@ -21,15 +21,14 @@ namespace StudentInformation
             while (continueFlag)
             {
                 Console.Write("Would you like to see a list of students? (y for yes): ");
-                string listAnswer = Console.ReadLine();
+                string listAnswer = YesOrNo(Console.ReadLine());
                 Console.WriteLine();
-                listAnswer = listAnswer.ToLower();
+
                 if (listAnswer == "y")
                 {
                     for (int i = 0; i < 11; i++)
                     {
-                        Console.WriteLine($"{i+1}. {studentNames[i]}");
-
+                        Console.WriteLine($"{i + 1}. {studentNames[i]}");
                     }
                     Console.WriteLine();
                 }
@@ -37,7 +36,6 @@ namespace StudentInformation
                 {
                     //HAHAHAHAHAHAHA
                 }
-
 
                 Console.Write($"Which student would you like to learn more about? (1-{arrayLength}): ");
                 int userInput = IntValidation(Console.ReadLine()); 
@@ -47,7 +45,7 @@ namespace StudentInformation
                 bool validInput = false;
                 while (!validInput)
                 {
-                    Console.WriteLine($"What would you like to know about {studentNames[userInput-1]}?");
+                    Console.WriteLine($"What would you like to know about {studentNames[userInput - 1]}?");
                     Console.Write("f for favorite food, or t for previous title: ");
                     string userAnswer = Console.ReadLine();
                     userAnswer = userAnswer.ToLower();
@@ -57,11 +55,11 @@ namespace StudentInformation
                         switch (userAnswer)
                         {
                             case "f":
-                                Console.WriteLine($"{studentNames[userInput-1]}'s favorite food is {studentFood[userInput-1]}.\n");
+                                Console.WriteLine($"{studentNames[userInput - 1]}'s favorite food is {studentFood[userInput - 1]}.\n");
                                 validInput = true;
                                 break;
                             case "t":
-                                Console.WriteLine($"{studentNames[userInput-1]}'s previous title is {studentOldTitle[userInput-1]}.\n");
+                                Console.WriteLine($"{studentNames[userInput - 1]}'s previous title is {studentOldTitle[userInput - 1]}.\n");
                                 validInput = true;
                                 break;
                             default:
@@ -73,18 +71,31 @@ namespace StudentInformation
                         Console.Write("Please enter a valid selection: ");
                     }
 
-                    Console.Write("Would you like to learn about a different student (y/n)?: ");
-                    string cont = Console.ReadLine();
+                    Console.Write($"Would you like to learn more about {studentNames[userInput - 1]} (y/n)?: ");
+                    string extra = YesOrNo(Console.ReadLine());
                     Console.WriteLine();
-                    cont = cont.ToLower();
-                    while (cont != "y" && cont != "n")
+
+                    if (extra == "y")
                     {
-                        Console.Write("Please enter valid input (y/n): ");
-                        cont = Console.ReadLine();
-                        cont = cont.ToLower();
-                        Console.WriteLine();
+                        switch (userAnswer)
+                        {
+                            case "t":
+                                Console.WriteLine($"{studentNames[userInput - 1]}'s favorite food is {studentFood[userInput - 1]}.\n");
+                                validInput = true;
+                                break;
+                            case "f":
+                                Console.WriteLine($"{studentNames[userInput - 1]}'s previous title is {studentOldTitle[userInput - 1]}.\n");
+                                validInput = true;
+                                break;
+                            default:
+                                break;
+                        }
                     }
-                          
+
+                    Console.Write("Would you like to learn about a different student (y/n)?: ");
+                    string cont = YesOrNo(Console.ReadLine());
+                    Console.WriteLine();
+
                     if (cont == "n")
                     {
                         continueFlag = false;
@@ -97,6 +108,20 @@ namespace StudentInformation
                 }
             }
             Console.WriteLine("OK BYEEEEEEEEE!!!!!!!!!");
+        }
+
+        private static string YesOrNo(string answer)
+        {
+            answer = answer.ToLower();
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write("Please enter valid input (y/n): ");
+                answer = Console.ReadLine();
+                answer = answer.ToLower();
+                Console.WriteLine();
+            }
+
+            return answer;
         }
 
         public static int IntValidation(string input)
